2aefb483f49bcd0d7d2de7899b26b0277af9fb4a
Urša Miklavčič <ursa.miklavcic@gmail.com>
2026-05-08 07:19:43 +0000

    update plot for functional modules in C. saudiense

--- code/r_code/07_DRAM.R
+++ code/r_code/07_DRAM.R
@@ -1,27 +1,27 @@
 # DRAM 
 
-
 library(dplyr)
 library(ggplot2)
 library(readxl)
 library(tidyr)
 library(stringr)
 
-summaries <- read_xlsx('005_DRAM/distill/metabolism_summary.xlsx') %>%  
-  pivot_longer(names_to = 'samples', values_to = 'PA', cols = starts_with('H00'))
+metadata <- read.table('data/metadata_genomes.tsv', header = T)
 
-metadata <- read.table('metadata_genomes.tsv', header = T)
+strains <- read_xlsx('005_DRAM/distill/metabolism_summary.xlsx') %>%  
+  pivot_longer(names_to = 'samples', values_to = 'PA', cols = starts_with('H00')) %>% 
+  mutate(PA = as.integer(as.numeric(PA)), 
+         samples = str_remove(samples, '_hybrid')) %>%  
+  filter(!samples %in% c('H005_94', 'H005_126')) %>% 
+  left_join(metadata, by = 'samples')
+  
 
-strains <- full_join(summaries, metadata, by = 'samples') %>%  
-  mutate(samples = str_remove(samples, '_hybrid'))
-
-strains_genes <- strains %>% 
-  group_by(module, samples, TIMEPOINT, ANTIBIOTIC_THERAPY, STRAIN_GROUP, STRAINS) %>% 
-  reframe(PA = sum(as.numeric(PA), na.rm = T))
-
-strains %>%  
-  filter(!is.na(module), PA > 0) %>% 
-  ggplot(aes(x = samples, y = module, fill = as.numeric(PA))) +
-  geom_tile() +
-  theme(axis.text.x = element_text(angle = 90))
+strains %>% 
+  group_by(STRAINS, module) %>% 
+  reframe(PA = ifelse(sum(PA) > 0, 1, 0)) %>% 
+  filter(PA > 0) %>% 
+  ggplot(aes(x = as.factor(STRAINS), y = module, fill = as.factor(PA))) +
+  geom_point(fill = 'black', alpha = .4, size = 5) +
+  labs(x = 'Strains', y = 'Module') +
+  theme_bw()
 ggsave('out/DRAM/modules_genome.svg', dpi=600)

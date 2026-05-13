6533465b911e35da34553fb61c1e1c8344f2372b
Urša Miklavčič <ursa.miklavcic@gmail.com>
2026-05-13 12:18:33 +0000

    Supplement table + figures

--- code/r_code/012_genomad.R
+++ code/r_code/012_genomad.R
@@ -32,8 +32,20 @@ viruses <- map_dfr(f_viruses, function(f) {
     mutate(genome = genome_name) }) %>% 
   left_join(metadata, by = join_by('genome' == 'samples'))
 
+# How long are geNomad viral sequences? 
+min(as.numeric(viruses$length))
+max(as.numeric(viruses$length))
 
-# Virus clusters based on MMseq2 (identity 90%, coverage 80%)
+viruses %>%  
+  ggplot(aes(x = as.numeric(length))) +
+  geom_histogram(binwidth = 1000) +
+  geom_vline(xintercept = 50000) +
+  labs(x = 'Viral sequence length [bp]', y = 'Number of sequences')
+ggsave('out/genomad/viral_sequence_length.png', dpi=400)
+
+
+
+# Virus clusters based on MMseq2 (identity 95%, coverage 80%)
 virus_clusters <- read_tsv('012_genomad/virus_mmseq2/virus_clusters.tsv', col_names = c('cluster', 'member')) %>%
   mutate(genome1 = str_extract(member, "H[0-9]+_[0-9]+"), 
          genome2 = str_extract(cluster, "H[0-9]+_[0-9]+"), 
@@ -41,10 +53,14 @@ virus_clusters <- read_tsv('012_genomad/virus_mmseq2/virus_clusters.tsv', col_na
   filter(!genome1 %in% c("H005_126", "H005_94"), 
          !genome2 %in% c("H005_126", "H005_94")) %>% 
   left_join(metadata, by = join_by('genome1' == 'samples')) %>% 
-  left_join(select(viruses, seq_name, topology, virus_score, taxonomy), by = 'seq_name') %>% 
-  filter(virus_score > 0.95)
+  left_join(select(viruses, seq_name, topology, virus_score, length, coordinates, n_genes, n_hallmarks, taxonomy), by = 'seq_name') %>% 
+  filter(virus_score > 0.95, genome1 != genome2)
 
-write.csv(virus_clusters, 'out/genomad/viral_clusters.csv')
+virus_clusters_supplement <- virus_clusters %>% 
+  group_by(seq_name, topology, virus_score, length, coordinates, n_genes, n_hallmarks, taxonomy) %>% 
+  reframe(isolates = paste(unique(c(genome1, genome2)), collapse = ","), 
+          strains = paste(unique(STRAINS), collapse = ','))
+write.csv(virus_clusters_supplement, 'out/genomad/virus_clusters_supplement.csv')
 
 # Count shared clusters and who has them?
 virus_clusters %>% 
@@ -78,29 +94,21 @@ virus_clusters %>%
 #   facet_wrap(~STRAINS, scales = 'free')
 
 # How to show across time 
-virus_clusters %>%  filter(STRAINS %in% c(6,7)) %>% 
-  ggplot(aes(y = seq_name, x = as.factor(TIMEPOINT),  fill = topology)) +
-  geom_tile() +
-  facet_wrap(~STRAINS, scales = 'free') +
-  labs(x = 'Time point', y = 'Viral cluster')
-ggsave('out/genomad/viruses_time_topology.png', dpi=600)
+virus_clusters %>%  #filter(STRAINS %in% c(6,7)) %>% 
+  ggplot(aes(y = seq_name, x = as.factor(STRAINS))) +
+  geom_point(size = 4, color = 'grey') +
+  labs(x = 'Strains', y = 'Viral cluster')
+ggsave('out/genomad/viruses_strains.png', dpi=600)
 
 length(unique(virus_clusters$seq_name))
 
-virus_clusters %>% 
-  ggplot(aes(y = genome1, x = as.factor(TIMEPOINT),  fill = seq_name)) +
-  geom_tile() +
-  facet_wrap(STRAINS~seq_name, scales = 'free') +
+virus_clusters %>%
+  ggplot(aes(y = reorder(seq_name, STRAINS), x = as.factor(TIMEPOINT), color = as.factor(STRAINS))) +
+  geom_point(size = 4, show.legend = F) +
+  facet_grid(~STRAINS, scales = 'free_x') +
   labs(x = 'Time point', y = 'Viral cluster')
+ggsave('out/genomad/viruses_time_strain.png', dpi = 600)
 
-virus_clusters %>% 
-  ggplot(aes(y = genome1, x = as.factor(TIMEPOINT),  fill = taxonomy)) +
-  geom_tile() +
-  facet_wrap(STRAINS~taxonomy, scales = 'free') +
-  labs(x = 'Time point', y = 'Viral cluster')
-  
-  
-  
 # Plasmids 
 # read plasmids into R 
 f_plasmids <- list.files(
@@ -119,6 +127,17 @@ plasmids <- map_dfr(f_plasmids, function(f) {
   left_join(metadata, by = join_by('genome' == 'samples')) %>% 
   filter(plasmid_score > 0.9)
 
+# How long are geNomad viral sequences? 
+min(as.numeric(plasmids$length))
+max(as.numeric(plasmids$length))
+
+plasmids %>%  
+  ggplot(aes(x = as.numeric(length))) +
+  geom_histogram(binwidth = 1000) +
+  geom_vline(xintercept = 50000) +
+  labs(x = 'Plasmid sequence length [bp]', y = 'Number of sequences')
+ggsave('out/genomad/plasmid_sequence_length.png', dpi=400)
+
 plasmid_clusters <- read_tsv('012_genomad/plasmid_mmseq2/plasmid_clusters.tsv', col_names = c('cluster', 'member')) %>%
   mutate(genome1 = str_extract(member, "H[0-9]+_[0-9]+"), 
          genome2 = str_extract(cluster, "H[0-9]+_[0-9]+"), 
@@ -126,28 +145,45 @@ plasmid_clusters <- read_tsv('012_genomad/plasmid_mmseq2/plasmid_clusters.tsv', 
   filter(!genome1 %in% c("H005_126", "H005_94"), 
          !genome2 %in% c("H005_126", "H005_94")) %>% 
   left_join(metadata, by = join_by('genome1' == 'samples')) %>% 
-  left_join(select(plasmids, seq_name, topology, plasmid_score), by = 'seq_name') %>% 
-  filter(plasmid_score > 0.95)
+  left_join(select(plasmids, seq_name, topology, plasmid_score, length, n_genes, n_hallmarks, conjugation_genes, amr_genes), by = 'seq_name') %>% 
+  filter(plasmid_score > 0.95, genome1 != genome2)
 
-plasmid_clusters %>% filter(STRAINS %in% c(6,7)) %>% 
-  ggplot(aes(y = seq_name, x = as.factor(TIMEPOINT), fill = topology)) +
-  geom_tile() +
-  facet_wrap(~STRAINS, scales = 'free') +
-  labs(x = 'Time point', y = 'Plasmid cluster')
-ggsave('out/genomad/plasmids_time_topology.png', dpi=600)
 
-length(unique(plasmid_clusters$seq_name))
+plasmid_clusters_supplement <- plasmid_clusters %>% 
+  group_by(seq_name, topology, plasmid_score, length, n_genes, n_hallmarks, conjugation_genes, amr_genes) %>% 
+  reframe(isolates = paste(unique(c(genome1, genome2)), collapse = ","), 
+          strains = paste(unique(STRAINS), collapse = ','))
+write.csv(plasmid_clusters_supplement, 'out/genomad/plasmids_clusters_supplement.csv')
 
+# Count shared clusters and who has them?
 plasmid_clusters %>% 
-  ggplot(aes(y = genome1, x = as.factor(TIMEPOINT),  fill = seq_name)) +
-  geom_tile() +
-  facet_wrap(STRAINS~seq_name, scales = 'free') +
+  group_by(cluster) %>% 
+  reframe(n_genomes = n_distinct(genome1), 
+          name_genomes = list(genome1))
+
+
+# How to show across time 
+plasmid_clusters %>%  #filter(STRAINS %in% c(6,7)) %>% 
+  ggplot(aes(y = reorder(seq_name, STRAINS), x = as.factor(STRAINS))) +
+  geom_point(size = 4, color = 'grey') +
+  labs(x = 'Strains', y = 'Plasmid cluster')
+ggsave('out/genomad/viruses_strains.png', dpi=600)
+
+length(unique(virus_clusters$seq_name))
+
+plasmid_clusters %>%
+  ggplot(aes(y = reorder(seq_name, STRAINS), x = as.factor(TIMEPOINT), color = as.factor(STRAINS))) +
+  geom_point(size = 4, show.legend = F) +
+  facet_grid(~STRAINS, scales = 'free_x') +
   labs(x = 'Time point', y = 'Plasmid cluster')
+ggsave('out/genomad/plasmid_time_strain.png', dpi = 600)
 
 
-# plasmid_clusters %>% 
-#   ggplot(aes(x = as.factor(TIMEPOINT), y = seq_name, fill = as.factor(STRAINS))) +
-#   geom_tile() + 
-#   facet_wrap(~topology, scales = 'free', nrow = 3) +
-#   labs(x = 'Time point', y = 'Plasmid cluster', fill = 'Strain')
-# ggsave('out/genomad/plasmid_heatmap.png')
+
+
+
+
+
+
+
+
